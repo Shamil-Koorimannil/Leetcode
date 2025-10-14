51b50ae542d896b234270fc6262fa54d0f214d67
Shamil Koorimannil <118761931+Shamil-Koorimannil@users.noreply.github.com>
2025-10-14 10:16:53 +0530

    Time: 1 ms (25.64%), Space: 152.8 MB (10.26%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int addDigits(int num) {
+    if(num<10){
+        return num;
+    }
+    int s=0;
+    while(num>0){
+        s+=num%10;
+        num~/=10;
+    }
+    return addDigits(s);
+    }
+  }
